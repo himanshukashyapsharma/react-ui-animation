8651e688c9e78f8c522338eb4b8652f4682e2c8c
himanshu <himanshu.sharma@healthmug.com>
2021-05-03 10:48:15 +0530

    charts animation

--- src/components/screen-components/dashboard-components/DealsChart.js
+++ src/components/screen-components/dashboard-components/DealsChart.js
@@ -1,46 +1,79 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
 import Chart from "react-apexcharts";
-import { animated } from 'react-spring'
+import { animated } from "react-spring";
 
+import { useTransition, config } from "react-spring";
 
-const DealsChart = ({DealsAnimation, slideLeftText}) => {
+const DealsChart = ({ DealsAnimation, slideLeftText }) => {
+	const [showChart, SetShowChart] = useState(false);
 
-    const [state, SetState] = useState({
-        series: [{
-            name: 'Deals',
-            data: [50, 155, 55, 150]
-          }],
-          options: {
-            chart: {
-              // height: 300,
-              width: '100%',
-              type: 'area'
-            },
-            dataLabels: {
-              enabled: false
-            },
-            stroke: {
-              curve: 'smooth'
-            },
-            xaxis: {
-              type: 'datetime',
-              categories: ["2018-12-01", "2018-12-08", "2018-12-16", "2018-12-30"]
-            },
-            tooltip: {
-              x: {
-                format: 'dd/MM/yy'
-              },
-            },
-          }
-    })
+	useEffect(() => {
+		setTimeout(() => {
+			SetShowChart(true);
+		}, 1500);
+	}, []);
 
-    return (<animated.div style={DealsAnimation} className="deals-wrapper">
-              <div className="chart-header">
-                <animated.span style={slideLeftText} className="font-15 black">Deals</animated.span>
-                <animated.span style={slideLeftText} className="chart-select">Show: Monthly</animated.span>
-              </div>
-                <Chart options={state.options} series={state.series} type="area" />
-          </animated.div>)
-}
+	const [state, SetState] = useState({
+		series: [
+			{
+				name: "Deals",
+				data: [50, 155, 55, 150],
+			},
+		],
+		options: {
+			chart: {
+				// height: 300,
+				width: "100%",
+				type: "area",
+			},
+			dataLabels: {
+				enabled: false,
+			},
+			stroke: {
+				curve: "smooth",
+			},
+			xaxis: {
+				type: "datetime",
+				categories: ["2018-12-01", "2018-12-08", "2018-12-16", "2018-12-30"],
+			},
+			tooltip: {
+				x: {
+					format: "dd/MM/yy",
+				},
+			},
+		},
+	});
 
-export default DealsChart
+	///////////////////
+	const transitions = useTransition(showChart, {
+		from: { opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
+		delay: 2000,
+		config: config.molasses,
+	});
+	///////////////////
+
+	return (
+		<animated.div style={DealsAnimation} className="deals-wrapper">
+			<div className="chart-header">
+				<animated.span style={slideLeftText} className="font-15 black">
+					Deals
+				</animated.span>
+				<animated.span style={slideLeftText} className="chart-select">
+					Show: Monthly
+				</animated.span>
+			</div>
+			{transitions(
+				(style, item) =>
+					item && (
+						<animated.div style={style}>
+							<Chart options={state.options} series={state.series} type="area" />
+						</animated.div>
+					)
+			)}
+		</animated.div>
+	);
+};
+
+export default DealsChart;
--- src/components/screen-components/dashboard-components/TasksChart.js
+++ src/components/screen-components/dashboard-components/TasksChart.js
@@ -1,41 +1,69 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
 import Chart from "react-apexcharts";
-import { animated } from 'react-spring'
+import { useTransition, config, animated } from "react-spring";
 
+const TasksChart = ({ TasksChartAnimation, slideLeftText }) => {
+	const [showChart, SetShowChart] = useState(false);
 
-const TasksChart = ({TasksChartAnimation, slideLeftText}) => {
+	useEffect(() => {
+		setTimeout(() => {
+			SetShowChart(true);
+		}, 2000);
+	}, []);
 
-    const [state, SetState] = useState({
-          
-        series: [26,59,15],
-        options: {
-            chart: {
-              type: 'donut',
-            },
-            labels: ['Active', 'Completed', 'Ended'],
-            responsive: [{
-              breakpoint: 480,
-              options: {
-                chart: {
-                  width: 200
-                },
-                legend: {
-                  position: 'bottom'
-                }
-              }
-            }]
-          },
-        
-        
-    })
-    
-    return (<animated.div style={TasksChartAnimation} className="tasks-pie-chart-wrapper">
-              <div className="chart-header">
-                <animated.span style={slideLeftText} className="font-15 black">Tasks</animated.span>
-                <animated.span style={slideLeftText} className="chart-select">Show: Monthly</animated.span>
-              </div>
-              <Chart options={state.options} series={state.series} type="donut" />
-          </animated.div>)
-}
+	const [state, SetState] = useState({
+		series: [26, 59, 15],
+		options: {
+			chart: {
+				type: "donut",
+			},
+			labels: ["Active", "Completed", "Ended"],
+			responsive: [
+				{
+					breakpoint: 480,
+					options: {
+						chart: {
+							width: 200,
+						},
+						legend: {
+							position: "bottom",
+						},
+					},
+				},
+			],
+		},
+	});
 
-export default TasksChart
+	///////////////////
+	const transitions = useTransition(showChart, {
+		from: { opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
+		delay: 2000,
+		config: config.molasses,
+	});
+	///////////////////
+
+	return (
+		<animated.div style={TasksChartAnimation} className="tasks-pie-chart-wrapper">
+			<div className="chart-header">
+				<animated.span style={slideLeftText} className="font-15 black">
+					Tasks
+				</animated.span>
+				<animated.span style={slideLeftText} className="chart-select">
+					Show: Monthly
+				</animated.span>
+			</div>
+			{transitions(
+				(style, item) =>
+					item && (
+						<animated.div style={style}>
+							<Chart options={state.options} series={state.series} type="donut" />
+						</animated.div>
+					)
+			)}
+		</animated.div>
+	);
+};
+
+export default TasksChart;
